0cdfb0be7c41079a85ed92f20067d5be88aff544
MuntherAlsaid <Muntheralsaid7@gmail.com>
2024-09-25 12:23:00 +0400

    Add files via upload

---
+++ Lab2.c
@@ -0,0 +1,54 @@
+/*Coffe machine simulator*/
+#include <stdio.h>
+
+/* define ingredient of espresso */
+#define esspresso_coffe_bean 8
+#define esspresso_water 30
+#define espresso_price 3.5
+
+/*Define ingredients of cappucino*/
+#define Cappuccino_coffe_bean 8
+#define Cappuccino_water 30
+#define Cappuccino_milk 70
+#define Cappuccino_price 4.5
+
+/*Define ingredients of mocha*/
+#define Mocha_coffe_bean 8
+#define Mocha_water 39
+#define Mocha_milk 160
+#define Mocha_choclate_syrup 30
+#define Mocha_price 5.5
+
+#define Admin_password 1234
+
+int current_beans
+
+char order(char coffe_type);
+
+int choice;
+
+void order_coffe();
+
+int main() 
+{
+    Printf("Welcome!\n");
+    printf("order a coffe press 1\n");
+    printf("Admin? press 2\n");
+    printf("exit! \n");
+    scanf("%d", &choice);
+
+    if (choice == 1);
+
+
+
+
+}
+
+void order_coffe(){
+    int coffe_choice;
+    printf(chose coffe type)
+
+
+}
+
+
